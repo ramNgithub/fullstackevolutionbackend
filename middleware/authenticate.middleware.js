29c47864ff8533e4c199a76b92c4e3bdb14ec2bc
Ram <ramnaphade86@gmail.com>
2023-02-20 17:58:21 +0530

    adsddd

--- middleware/authenticate.middleware.js
+++ middleware/authenticate.middleware.js
@@ -2,14 +2,20 @@ const jwt=require("jsonwebtoken");
 
 
 const authenticate=(req,res,next)=>{
+
+    
     const token=req.headers.authorization
     if(token){
         const decoded = jwt.verify(token, 'masai');
+
+
         if(decoded){
             const userID=decoded.userID
             req.body.userID=userID
             next()
         }else{
+
+
             res.send("Login first")
         }
     }else{
